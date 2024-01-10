6621f8c8ac65a59d052d811aab267a5a5176d433
Rahul Sharma <sharmarahul8848@gmail.com>
2024-01-10 10:47:42 +0530

    Project Setup

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <a>
+        <img src="" />
+        <h3>Photofolio</h3>
+      </a>
+    </div>
+  );
+}
